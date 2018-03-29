a1a54351f81879df35f5af153be5f2f52cd83c86
Brandon Allen <gbrandallen@gmail.com>
2018-03-28 22:40:50 -0700

    Resource Template Update

--- single-resource.php
+++ single-resource.php
@@ -5,12 +5,30 @@
   <div class="lkm-slab resource-nav">
     <div class="content-container lkm-row">
       <div class="lkm-column three-fourths nav-text-content">
-        <p><span>YOUR PROBLEMS ARE OUR PROBLEMS.</span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+        <?php
+          $postType = get_post_type_object(get_post_type());
+          if ($postType) {
+            ?>
+           <h4><?php echo esc_html($postType->labels->singular_name); ?></h4>
+        <?php  } ?>
+        <h1><?php the_title(); ?></h1>
       </div>
       <div class="lkm-column one-fourth nav-container">
-        <?php wp_nav_menu( array( 'theme_location' => 'practice-menu' ) ); ?>
+        <?php wp_nav_menu( array( 'theme_location' => 'resource-menu' ) ); ?>
       </div>
     </div>
+  </div>
+
+  <div class="lkm-slab">
+    test
+    <?php
+    echo 'test ' . count(get_the_category());
+      $cats = get_the_category();
+      for($i=0; $i<count($cats); $i++) {
+          echo 'category: ' . $cats[$i];
+      }
+    ?>
+
   </div>
 
 <footer class="footer">
